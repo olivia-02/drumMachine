e02cf2faec121bf8707643050a3b3aa320d60fad
olivia_02 <robson.livi@gmail.com>
2020-12-11 18:43:59 -0500

    Complete utility of first row (kick), with listeners and interval loop through.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-var arr = [0,1,2,3,4];
+debugger
+
+let kicks = document.querySelectorAll(`#kick`)
 var index = 0;
+
+
+function clickable(event) {
+	var clicked = event.target;
+	if (clicked.classList.contains(`activated`)) {
+		this.classList.remove(`activated`)
+	} else {
+		clicked.classList.add(`activated`);
+	}
+	};
+
 setInterval(function(){
-    console.log(arr[index++ % arr.length]);
-}, 1000)
+	console.log(kicks[index]);
+	kicks[index].setAttribute("style", "background-color: red;")
+	if (index >= 1){
+		kicks[index-1].style.backgroundColor = null;
+	} else {
+		kicks[7].style.backgroundColor = null;
+	}
+	if (kicks[index].classList.contains(`activated`)){
+		var kicksound = document.createElement('audio');
+  		kicksound.src = `/sounds/CYCdh_KesKick-08.wav`;
+  		document.body.appendChild(kicksound);
+  		kicksound.play();
+  		kicksound.onended = function () {
+    	this.parentNode.removeChild(this);
+  		}
+	}
+	index++;
+	console.log(index);
+	if (index === kicks.length){
+		index = 0;
+	};
+}, 500)
+
+function boysen(){
+	
+}
